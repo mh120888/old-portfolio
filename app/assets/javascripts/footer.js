15e20504df1ca9dbee2634ace40aee741429a280
mh120888 <matthew.alan.higgins@gmail.com>
2014-03-11 12:49:35 -0700

    Adds callbacks for clicking on Portfolio and Contact links Changes background image to colorful squares Makes main content area smaller Removes border from portfolio div Adds margin to sides of portfolio div

---
+++ app/assets/javascripts/footer.js
@@ -0,0 +1,15 @@
+// $( document ).ready(function() {
+//   footerView.bindEvents();
+// });
+
+// var footerView = {
+//   bindEvents: function() {
+//     $('.fa-google-plus-square').hover(footerView.fontAwesomeBigger, footerView.fontAwesomeSmaller);
+//   },
+//   fontAwesomeBigger: function() {
+//     debugger;
+//   },
+//   fontAwesomeSmaller: function() {
+//     console.log('out');
+//   }
+// }
